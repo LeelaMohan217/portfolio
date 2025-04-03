6f69fc0166e51444189821bedb227f8c7260ba39
NethalaM <Mohan.Nethala@cevalogistics.com>
2025-04-03 19:43:37 +0530

    few changes added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,10 @@ const Navbar = () => {
     <>
       <nav className="mb-20 flex items-center justify-between py-6">
         <div className="flex flex-shrink-0 items-center">
-          <img className="mx-2 w-10" src={logo} alt="logo" />
+          <p className="mx-2 font-bold text-2xl bg-gradient-to-r from-pink-300 via-slate-50 to-purple-500 bg-clip-text tracking-tight text-transparent">
+            My Portfolio
+          </p>
+          {/* <img className="mx-2 w-10" src={logo} alt="logo" /> */}
         </div>
         <div className="flex m-8 items-center justify-center gap-4 text-2xl text-white">
           <FaLinkedin />
